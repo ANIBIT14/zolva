a1c5c99b8f29b18ea8cbd9b9b0d7fc851597fa19
anibit14 <aniruddha.agarwal18@gmail.com>
2026-07-18 19:53:18 +0530

    chore: v0.5.0

--- src/zolva/__init__.py
+++ src/zolva/__init__.py
@@ -21,7 +21,7 @@ from zolva.signing import SignatureError, sign_payload, verify_zolva_signature
 from zolva.synthetics import SyntheticResult, SyntheticRunner, load_synthetics
 from zolva.tools import ToolRegistry, default_registry, tool
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 
 __all__ = [
     "BLOCKED_MESSAGE",
